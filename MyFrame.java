eb2669807e5df5066164e8ecd9ffbb79633ae2c1
Erlein Nicole <erleinureta@gmail.com>
2023-07-20 11:52:22 +0800

    threading to execute FX with Swing

--- MyFrame.java
+++ MyFrame.java
@@ -2,6 +2,10 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
 public class MyFrame extends JFrame{
     MyPanel aPanel;
@@ -21,15 +25,11 @@ public class MyFrame extends JFrame{
         MenuBar menuBar = new MenuBar();
 
         Menu pRoom = new Menu("Paint Room");
-        pRoom.addActionListener(new ActionListener() {
+        pRoom.addActionListener(new java.awt.event.ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e){
-                try{
-                    
-                }
-                catch(Exception e){
-                    e.printStackTrace();
-                }
+            public void actionPerformed(java.awt.event.ActionEvent e){
+                openFxmlFile("PaintScene.fxml");
+                System.out.println("Print me");
             }
         });
         Menu exit = new Menu("Bye");
@@ -38,4 +38,30 @@ public class MyFrame extends JFrame{
 
         this.setMenuBar(menuBar);
     }
+
+    private void openFxmlFile(String filename) {
+        try {
+            SwingUtilities.invokeLater(() -> {
+                JFXPanel fxPanel = new JFXPanel(); // Initialize JavaFX on the EDT
+                System.out.println("open fxml is working");
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(filename));
+                    Pane root = fxmlLoader.load();
+                    aPanel.removeAll(); // Remove the existing components from the panel
+                    aPanel.setLayout(new BorderLayout());
+                    aPanel.add(fxPanel, BorderLayout.CENTER); // Add the JFXPanel
+                    aPanel.revalidate();
+    
+                    // Create a JavaFX scene and set the content
+                    Scene scene = new Scene(root);
+                    fxPanel.setScene(scene);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
 }
